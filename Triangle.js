3fb22c1fb58c8c1315343302627f8420fad7381f
Geratha <kandenzi018@gmail.com>
2018-12-14 15:23:36 +0200

    link of css

--- Triangle.js
+++ Triangle.js
@@ -1,16 +1,25 @@
-function myFunction(){
+function myTriangle(){
+     
     var number1=parseInt(document.getElementById("number1").value);
     var number2=parseInt(document.getElementById("number2").value);
     var number3=parseInt(document.getElementById("number3").value);
 
-            function Triangle(){
-                if( number1==number2 && number1==number3 && number2==number3);
-                console.log (alert ("Equilateral"));}
 
-                elseif (number1===number2 ||number1===number3 || number2<number3) {
-                    console.log (alert("esoceles"));}
-                    elseif( number1!=number2 && number1!=number3 && number2!=number3);
+            
+                if( number1==number2 && number1==number3 && number2==number3)
+                {
+                console.log (alert ("Equilateral"));
+                 }
+
+                else if (number1===number2 ||number1!=number3) 
+                {
+                    console.log (alert("esoceles"));
+                }
+                    else if( number1!=number2 && number1!=number3 && number2!=number3);
+                    {
                     console.log (alert("scalen"));
                 }
+                s
+            }
       
                 
