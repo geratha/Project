5263252f6c3fbf35cb9516fe23245bbd5f952033
Geratha <kandenzi018@gmail.com>
2018-12-14 12:17:08 +0200

    function

--- Triangle.js
+++ Triangle.js
@@ -3,3 +3,12 @@ function myFunction(){
     document.getElementById("myNumber").value;
             alert(firstString);
             }
+            function Triangle(number1, number2, number3){
+                if( number1==number2 && number1==number3 && number2==number3);{
+                    ("alert Equilateral")}
+            }
+            elseif(number1==number2 && number1==number3 && number2!=number3);{
+                ("alert isoceles");}
+                if(number1!=number2 || number1!=number3 || number2!=number3);{
+                    ("alert scalen")
+                }
