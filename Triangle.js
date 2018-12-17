9273adbc07e079055c16aa61f2fae5326556a326
Geratha <kandenzi018@gmail.com>
2018-12-17 19:03:58 +0200

    array

--- Triangle.js
+++ Triangle.js
@@ -1,27 +1,27 @@
-function myTriangle(){
-     
-    var number1=parseInt(document.getElementById("number1").value);
-    var number2=parseInt(document.getElementById("number2").value);
-    var number3=parseInt(document.getElementById("number3").value);
+function myTriangle() {
+
+    var number1 = parseInt(document.getElementById("number1").value);
+    var number2 = parseInt(document.getElementById("number2").value);
+    var number3 = parseInt(document.getElementById("number3").value);
+    var triangle = [number1, number2,number3];
+    console.log(triangle);
 
 
-            if(number1+number2>number3 && number1+number3>number2 && number2+number3>number1){
+    if (number1 + number2 > number3 && number1 + number3 > number2 && number2 + number3 > number1) {
 
-                if( number1==number2 && number1==number3 && number2==number3)
-                {
-                console.log (alert ("Equilateral"));
-                 }
-
-                else if (number1===number2 ||number1===number3 || number2===number3) 
-                {
-                    console.log (alert("esoceles"));
-                }
-                    else if( number1+number2>number3 || number2+number3>number1 || number1+number3>number2){
-                    
-                    console.log (alert("scalen"));
-                    }
-                }
-            else{
-                   cansole.log("non triangle");
-            };
+        if (number1 == number2 && number1 == number3 && number2 == number3) {
+            console.log(alert("Equilateral"));
         }
+
+        else if (number1 === number2 || number1 === number3 || number2 === number3) {
+            console.log(alert("esoceles"));
+        }
+        else if (number1 + number2 > number3 || number2 + number3 > number1 || number1 + number3 > number2) {
+
+            console.log(alert("scalen"));
+        }
+    }
+    else {
+        cansole.log("non triangle");
+    };
+}
